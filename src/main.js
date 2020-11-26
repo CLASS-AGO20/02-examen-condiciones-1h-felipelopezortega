cf12206369b496121b847195f582a46a8b01e6f9
felipelopezortega <flopezortega@ucol.mx>
2020-11-26 15:10:22 -0600

    function convertir added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 export default class App {
 
+    convertir(pesos){
+
+        if(pesos<2000){
+
+            return pesos/ 20.5
+        }else if(pesos>=2000){
+
+            return pesos/ 22.7
+        }
+    }
 }
 
 let app = new App();
+
+//Función de pesos a dólares o euros
+console.log(app.convertir(1999))
+console.log(app.convertir(2000))
+console.log(app.convertir(10000))
